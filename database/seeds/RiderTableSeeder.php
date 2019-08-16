2497e7e00e89a2bd9b123204992addf3cb643aa9
Emmanuel Audu <eaudu@gidimobile.com>
2019-08-16 09:12:27 +0100

    implemented riders seeder for test

---
+++ database/seeds/RiderTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RiderTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('rider')->insert([
+            'rider_first_name' => 'Mike', 
+            'rider_password' => app('hash')->make('mike96'), 
+            'rider_last_name' => 'Peter', 
+            'rider_user_name' => 'mike'
+        ]);
+
+        DB::table('rider')->insert([
+            'rider_first_name' => 'Emmanuel', 
+            'rider_password' => app('hash')->make('emma96'), 
+            'rider_last_name' => 'Audu', 
+            'rider_user_name' => 'emma'
+        ]);
+    }
+}
