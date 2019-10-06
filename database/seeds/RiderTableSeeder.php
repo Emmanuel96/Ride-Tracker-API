5b62d3c4739755bac529dc177f8744e467bb1d87
Emmanuel Audu <eaudu@gidimobile.com>
2019-10-06 15:04:21 +0100

    seeds

--- database/seeds/RiderTableSeeder.php
+++ database/seeds/RiderTableSeeder.php
@@ -12,30 +12,30 @@ class RiderTableSeeder extends Seeder
      */
     public function run()
     {
-        //for every rider, there should be a user created
+        // for every rider, there should be a user created
 
-        // DB::table('users')->insert([
-        //     'email' => 'mike@wailodile.com',
-        //     'password' => app('hash')->make('mike96'),
-        //     'name' => 'Mike Peter',
-        //     'rider_id' => 1
-        // ]);
+        DB::table('users')->insert([
+            'email' => 'sumai@wailo.com',
+            'password' => app('hash')->make('sumai96'),
+            'name' => 'Sumaila',
+            'rider_id' => 1
+        ]);
 
-        // DB::table('rider')->insert([
-        //     'rider_id' => 1,
-        //     'created_at' => Carbon::now()->format('Y-m-d H:i:s')
-        // ]);
+        DB::table('rider')->insert([
+            'rider_id' => 1,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
 
-        // DB::table('users')->insert([
-        //     'email' => 'emmanuel@wailodile.com',
-        //     'password' => app('hash')->make('emmanuel96'),
-        //     'name' => 'Emmanuel Audu',
-        //     'rider_id' => 2
-        // ]);
+        DB::table('users')->insert([
+            'email' => 'emmanuel@wailodile.com',
+            'password' => app('hash')->make('emmanuel96'),
+            'name' => 'Emmanuel Audu',
+            'rider_id' => 2
+        ]);
 
-        // DB::table('rider')->insert([
-        //     'rider_id' => 2,
-        //     'created_at' => Carbon::now()->format('Y-m-d H:i:s')
-        // ]);
+        DB::table('rider')->insert([
+            'rider_id' => 2,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
     }
 }
