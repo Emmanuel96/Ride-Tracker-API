c8b0155004edac54c36a257557bc58f89ed2cead
Emmanuel Audu <eaudu@gidimobile.com>
2019-10-07 08:06:44 +0100

    test to fix login controller auth method

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,26 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Auth; 
+use Auth;
 
 class LoginController extends Controller
 {
     public function postLogin(Request $request){
         $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials)) {
+
+        if (Auth::attempt(['user_email' => $request->email, 'user_password' => $request->password])) {
             // Authentication passed...
             $response = [
-                'auth_user'=>Auth::user(), 
+                'auth_user'=>Auth::user(),
                 'text' => 'User has been authenticated successfully'
             ];
-            return $response;  
+            return $response;
         }
         else{
             $response = [
-                'auth_user' => 'Credentials do not match', 
+                'auth_user' => 'Credentials do not match',
                 'text' => 'Credentials do not match'
             ];
+
         }
     }
 }
