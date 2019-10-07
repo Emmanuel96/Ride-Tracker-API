c64f7ed33746912849c4f7cb3daf5f849bad57b4
Emmanuel Audu <eaudu@gidimobile.com>
2019-10-07 08:16:42 +0100

    modified the user model

--- app/User.php
+++ app/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'user_password', 'remember_token',
     ];
 
     /**
